b4632f473df67f0f523fb00f41d41e2fcd7d5c17
KimJunHyeok <denshol0709@gmail.com>
2024-04-15 11:44:05 +0900

    폴더정리 후 커밋

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,47 +1,10 @@
-import { NavermapsProvider } from "react-naver-maps";
-
-import React, { useEffect, useState } from "react";
-import DaumPostcode from "react-daum-postcode";
-
-import UndongMap from "./TrainerMap/UndongMap";
-import TrainerList from "./TrainerMap/TrainerList";
+import React from "react";
+import TrainerMap from "./components/mainpage/trainermap/TrainerMap";
 
 function App() {
-  
-
-  const [searchPosition, setSearchPosition] = useState({
-    lat: 37.3595704,
-    lng: 127.105399,
-  }); // [latitude, longitude
-
-  const [address, setAddress] = useState("");
-
-  const handleComplete = (data) => {
-    let fullAddress = data.address;
-    let extraAddress = "";
-
-    if (data.addressType === "R") {
-      if (data.bname !== "") {
-        extraAddress += data.bname;
-      }
-      if (data.buildingName !== "") {
-        extraAddress +=
-          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
-      }
-      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
-    }
-
-    console.log(fullAddress);
-    setAddress(fullAddress);
-  };
-
   return (
     <>
-      <TrainerList />
-      <NavermapsProvider ncpClientId="0dfie9x7ty" submodules={["geocoder"]}>
-        <UndongMap address={address} />
-      </NavermapsProvider>
-      {/* <DaumPostcode onComplete={handleComplete} />; */}
+      <TrainerMap />
     </>
   );
 }
