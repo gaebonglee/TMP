2da3371b2a4499228fe3b02e55a25797bbf38c2a
KimJunHyeok <denshol0709@gmail.com>
2024-04-17 17:29:29 +0900

    필터카테고리 껍데기 css추가

--- front-end/src/components/trainermap/FilterList.jsx
+++ front-end/src/components/trainermap/FilterList.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FilterList.scss";
 import { RiArrowGoBackLine } from "react-icons/ri";
-import MultiStepProgress from "./MultiStepProgress";
 
 const FilterList = (props) => {
-  let roundBox = document.querySelectorAll(".round_box");
-  roundBox.forEach((box) => {
-    box.addEventListener("click", () => {
-      box.classList.toggle("active");
-    });
-  });
+  const [value, setValue] = useState(500);
+  const [priceValue, setPriceValue] = useState(4);
+
+  const [comfort, setComfort] = useState([
+    "운동복 대여",
+    "무료주차",
+    "개인 락커",
+  ]);
+
+  const handleChange = (event) => {
+    setValue(Number(event.target.value));
+  };
+  function handleChangePrice(event) {
+    setPriceValue(Number(event.target.value));
+  }
+
+  function handleClass(e) {
+    e.target.classList.toggle("active");
+    console.log(e.target.textContent);
+  }
 
   return (
     <div className="FilterList">
@@ -23,6 +36,9 @@ const FilterList = (props) => {
         </div>
         <div
           style={{ fontSize: "15px", fontWeight: "bolder", cursor: "pointer" }}
+          onClick={() => {
+      
+          }}
         >
           필터 초기화
         </div>
@@ -32,7 +48,8 @@ const FilterList = (props) => {
         <div className="SORT">정렬</div>
         <div className="sort">
           <div className="defaultSort">
-            <input type="radio" name="sort" id="defaultSort" /> 기본순
+            <input type="radio" name="sort" id="defaultSort" defaultChecked />
+            기본순
           </div>
           <div className="meterSort">
             <input type="radio" name="sort" id="meterSort" />
@@ -47,23 +64,39 @@ const FilterList = (props) => {
       <div className="searchRound">
         <div className="searchRoundTitle">
           <div>검색반경</div>
-          <div>NNNm 이내</div>
+          <div>{value}m 이내</div>
         </div>
         <div>
-          <MultiStepProgress />
+          <input
+            type="range"
+            name=""
+            id="searchRangeBar"
+            max={3000}
+            min={500}
+            step={500}
+            onChange={handleChange}
+          />
         </div>
         <div className="meter">
-          <div>NNNm</div>
-          <div>Nkm</div>
+          <div>500m</div>
+          <div>3km</div>
         </div>
       </div>
       <div className="priceRound">
         <div className="priceRoundTitle">
           <div>가격</div>
-          <div>4만원 이내</div>
+          <div>{priceValue}만원 이내</div>
         </div>
         <div>
-          <progress value="50" max="200"></progress>
+          <input
+            type="range"
+            name=""
+            id="priceRangeBar"
+            min={4}
+            max={20}
+            step={1}
+            onChange={handleChangePrice}
+          />
         </div>
         <div className="price">
           <div>4만원</div>
@@ -74,7 +107,7 @@ const FilterList = (props) => {
         <div className="GENDER">성별</div>
         <div className="genderCheck">
           <div>
-            <input type="radio" name="gender" id="all" />
+            <input type="radio" name="gender" id="all" defaultChecked />
             전체
           </div>
           <div>
@@ -90,9 +123,11 @@ const FilterList = (props) => {
       <div className="comfort">
         <div>이용편의</div>
         <div className="comfortBox">
-          <div className="round_box">운동복 대여</div>
-          <div className="round_box">무료주차</div>
-          <div className="round_box">개인 락커</div>
+          {[...comfort].map((comfort, index) => (
+            <div key={index} className="round_box" onClick={handleClass}>
+              {comfort}
+            </div>
+          ))}
         </div>
       </div>
       <div className="btn_area">
--- front-end/src/components/trainermap/TrainerListItem.jsx
+++ front-end/src/components/trainermap/TrainerListItem.jsx
@@ -1,134 +1,36 @@
 import React from "react";
 import { TbMapPin } from "react-icons/tb";
 import { BsFillStarFill } from "react-icons/bs";
-import "../../common.scss";
+import "./TrainerListItem.scss";
 
 const TrainerListItem = () => {
   return (
-    <div
-      className="TrainerListItem"
-      style={{
-        marginBottom: 17,
-        width: 491,
-        height: 198,
-        position: "relative",
-        background: "white",
-        borderRadius: 24,
-        overflow: "hidden",
-        border: "2px #00491e solid",
-      }}
-    >
-      <div
-        style={{
-          left: 178,
-          top: 24,
-          position: "absolute",
-          color: "black",
-          fontSize: 16,
-          fontFamily: "Inter",
-          fontWeight: "600",
-
-          wordWrap: "break-word",
-        }}
-      >
-        @@@ 선생님
-      </div>
-      <div
-        className="3050000"
-        style={{
-          width: 283,
-          height: 57,
-          left: 178,
-          top: 54,
-          position: "absolute",
-        }}
-      >
-        <span
-          style={{
-            color: "black",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "600",
-
-            wordWrap: "break-word",
-          }}
-        >
+    <div className="TrainerListItem">
+      <div className="morae">@@@ 선생님</div>
+      <div className="moraeTitle">
+        <span className="moraeTitleText">
           [서울 피티 대표] <br />
           ~~스포츠모델 그랑프리
           <br />
           <br />
         </span>
-        <span
-          style={{
-            color: "#6D6D6D",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "600",
-
-            wordWrap: "break-word",
-          }}
-        >
-          30회 기준 회당{" "}
-        </span>
-        <span
-          style={{
-            color: "#6D6D6D",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "800",
-
-            wordWrap: "break-word",
-          }}
-        >
+        <span className="moraeTitleTextPT">30회 기준 회당 </span>
+        <span className="moraePrice">
           50,000원
           <br />
         </span>
       </div>
-      <div
-        className="Pt10"
-        style={{
-          height: 19,
-          left: 178,
-          top: 145,
-          position: "absolute",
-          color: "#6D6D6D",
-          fontSize: 15,
-          fontFamily: "Inter",
-          fontWeight: "600",
-          marginTop: 5,
-          wordWrap: "break-word",
-        }}
-      >
+      <div className="moraeLocation">
         <TbMapPin size={17} />
         서울 PT샵 강남점 - 강남역 보도 10분
       </div>
-      <div
-        className="Nn"
-        style={{
-          height: 23,
-          left: 380,
-          top: 24,
-          position: "absolute",
-          color: "black",
-          fontSize: 16,
-          fontFamily: "Inter",
-          fontWeight: "600",
-
-          wordWrap: "break-word",
-        }}
-      >
+      <div className="moraeReview">
         <BsFillStarFill size={20} color="rgb(255,187,51)" />
         후기 NN개
       </div>
       <img
-        className="trainerimg"
-        style={{
-          width: 160,
-          height: 199,
-          left: 0,
-          top: 0,
-          position: "absolute",
-        }}
+        className="trainerImg"
+        
         src="img/morae.jpg"
         alt="1"
       />
