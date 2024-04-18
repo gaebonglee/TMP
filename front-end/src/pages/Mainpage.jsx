1965b3df3607ae5c18c74d129d221255b6851bb3
gaebonglee <ken604@naver.com>
2024-04-18 11:40:41 +0900

    pages 내용수정

--- front-end/src/pages/Mainpage.jsx
+++ front-end/src/pages/Mainpage.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import Header from "../components/layout/Header";
-import Footer from "../components/layout/Footer";
 import Search from "../components/mainpage/Search";
 import TrainerCard from "../components/mainpage/TrainerCard";
-
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <>
       <Search />
       <TrainerCard />
-      <Footer />
-    </div>
+    </>
   );
 };
 
