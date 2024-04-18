f1727a11f9544fd1237b9a2aecfd74661c1a04b3
Jimmy <skawk3235@gmail.com>
2024-04-18 10:34:52 +0900

    db connection 설정

---
+++ back-end/connection/mysqlConnection.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../../.env' });
+
+// 데이터베이스 연결 정보
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+});
+
+// 데이터베이스 연결
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database as id ' + connection.threadId);
+});
+
+module.exports = connection;
