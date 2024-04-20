39dd45fb6bc4d299c432b9e1f5c43c8ab3363950
Jimmy <skawk3235@gmail.com>
2024-04-20 11:36:57 +0900

    App.js 경로 수정

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,8 +16,6 @@ function App() {
         { index: true, element: <Mainpage /> },
         { path: "/trainermap", element: <TotalTrainerMap /> },
         { path: "/centermap", element: <TotalCenterMap /> },
-
-        { path: "/TrainerDetail", element: <TrainerDetail /> },
       ],
     },
   ]);
