a2777e7419ed0ba0ba694954d73d02e4c7b319cc
KimJunHyeok <denshol0709@gmail.com>
2024-04-20 14:59:38 +0900

    후기 modal 제작중

--- front-end/src/components/trainerdetail/detailLeft/left/Review.jsx
+++ front-end/src/components/trainerdetail/detailLeft/left/Review.jsx
@@ -83,6 +83,131 @@ const Review = () => {
           </div>
         </div>
       </div>
+      <div
+        tabIndex={-1}
+        style={{
+          position: "relative",
+          zIndex: 1050,
+          display: "block",
+        }}
+      >
+        <div>
+          <div
+            className="modal fade show"
+            role="dialog"
+            tabIndex={-1}
+            style={{
+              display: "block",
+            }}
+          >
+            <button className="close">X</button>
+          </div>
+          <div className="modal-backdrop fade show">
+            <div className="modal-dialog wideModal FullModal">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <div className="modal-component">
+                    <h5 className="headerTitle">
+                      "솔직한 후기를 남겨주세요!"
+                      <div className="exitButton">엑스아이콘</div>
+                    </h5>
+                    <div className="reviewWrap">
+                      <label className="reviewTitle">
+                        "일반이용 후기" "작성 요령"
+                      </label>
+                    </div>
+                    <ul className="help">
+                      <li>
+                        • 해당 코치님에게 트레이닝을 받은 증빙사진이 필요합니다.
+                        (PT 약정서/계약서 or 코치님과 함께 찍은 사진 등 기타)
+                      </li>
+                      <li>
+                        • 증빙사진의 공개/비공개 여부를 선택할 수 있습니다.
+                      </li>
+                      <li>
+                        • 업로드된 사진을 터치/드래그 하여 삭제/이동 할 수
+                        있습니다.
+                      </li>
+                      <li>
+                        • 증빙이 적절하지 않을 시 삭제처리 됩니다.(상시 모니터링
+                        중)
+                      </li>
+                    </ul>
+                    <div style={{ marginTop: "12px" }}>
+                      <div className="flexBox reviewWrap">
+                        <div className="ReviewEditModal">
+                          <label className="reviewTitle">
+                            별점을 선택해주세요
+                          </label>
+                          <div>
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                            <FaStar />
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <div className="flexBox reviewWrap">
+                          <label className="reviewTitle">
+                            증빙사진을 올려주세요 (최대 3개)
+                          </label>
+                          <div className="flexBox">
+                            <input type="checkbox" name="" id="" checked />
+                            <label for="check_photo"></label>
+                            <label
+                              for="check_photo"
+                              className="reviewPhotoAuth"
+                            >
+                              사진 공개
+                            </label>
+                          </div>
+                        </div>
+                        <label for="review_filename">
+                          <div className="addPhotoButton">+ 사진 추가하기</div>
+                        </label>
+                        <input
+                          type="file"
+                          name="p_image_1"
+                          id="review_filename"
+                        />
+                      </div>
+                      <div style={{ margin: "24px 0px" }}>
+                        <div className="reviewWrap">
+                          <label className="reviewTitle">
+                            내용을 작성해주세요.
+                            <div
+                              style={{
+                                float: "right",
+                                fontSize: "14px",
+                                color: "rgb(108,118,128)",
+                              }}
+                            >
+                              0/400
+                            </div>
+                          </label>
+                        </div>
+                        <textarea
+                          name="content"
+                          maxLength={400}
+                          placeholder="내용을 입력해주세요"
+                          rows="6"
+                        ></textarea>
+                      </div>
+                      <div className="button_Div">
+                        <button className="button_Basic">
+                          <span>리뷰 작성 완료</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
