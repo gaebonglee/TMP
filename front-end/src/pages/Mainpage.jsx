7925ec6241d7a8342128791f4f5b08a799a1a202
Jimmy <skawk3235@gmail.com>
2024-04-23 17:15:12 +0900

    main page에서 photoViewer테스트

--- front-end/src/pages/Mainpage.jsx
+++ front-end/src/pages/Mainpage.jsx
@@ -3,11 +3,13 @@ import Search from "../components/mainpage/Search";
 import TrainerCard from "../components/mainpage/TrainerCard";
 import LoginErrorModal from "../components/loginErrorModal/LoginErrorModal";
 import { useParams } from "react-router-dom";
+import PhotoViewer from "../components/photoViewer/PhotoViewer";
 const Home = () => {
   const { role } = useParams();
   return (
     <>
       {role && <LoginErrorModal roles={role} />}
+      <PhotoViewer />
       <Search />
       <TrainerCard />
     </>
