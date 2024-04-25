904c72026d01818aa0869afc28c606c6d06de45e
Jimmy <skawk3235@gmail.com>
2024-04-25 11:08:32 +0900

    route 수정

--- front-end/src/components/privateRoute/PrivateRoute.jsx
+++ front-end/src/components/privateRoute/PrivateRoute.jsx
@@ -1,7 +1,14 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, token, role }) => {
-  return token.role === role ? Component : <Navigate to="/"></Navigate>;
+  let result = false;
+
+  if (token.role !== undefined) {
+    if (role === undefined || token.role === role) {
+      result = true;
+    }
+  }
+  return result ? Component : <Navigate to="/"></Navigate>;
 };
 
 export default PrivateRoute;
