8713ffd1907ed414496e0dbc641a7358216a42d7
YeonuJung <gsh95214@naver.com>
2024-04-26 16:26:22 +0900

    selectCoach 쿼리문 수정

--- back-end/sql/mypage/trainer/selectCoach.js
+++ back-end/sql/mypage/trainer/selectCoach.js
@@ -3,7 +3,7 @@ const mysql = require("../../../connection/mysqlConnection");
 function selectCoach(user_id) {
   // Define the SQL query
   console.log(user_id)
-  const sql = `SELECT u.user_name, u.email, u.phonenumber, u.user_id, t.trainning_type, c.center_name, c.center_id FROM user u JOIN trainer t ON u.user_id = t.user_id JOIN center c ON t.center_id = c.center_id where u.user_id = ?`;
+  const sql = `SELECT u.user_name, u.email, u.phonenumber, u.user_id, t.trainning_type, c.center_name, c.center_id FROM user u JOIN trainer t ON u.user_id = t.user_id LEFT JOIN center c ON t.center_id = c.center_id where u.user_id = ?`;
 
   // Return a new promise that encapsulates the asynchronous operation
   return new Promise((resolve, reject) => {
