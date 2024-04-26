9222ebcc2b5827bdf3d5dcc796f9f8c822781ea6
jimmy <skawk3235@gmail.com>
2024-04-26 15:35:08 +0900

    left section 테스트

--- front-end/src/components/trainerdetail/detailLeft/LeftSection.jsx
+++ front-end/src/components/trainerdetail/detailLeft/LeftSection.jsx
@@ -7,7 +7,21 @@ import Review from "./left/Review";
 import LessonPrice from "./left/LessonPrice";
 import CenterPlace from "./left/CenterPlace";
 import "./LeftSection.scss";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../../trainermap/LoadingSpinner";
 const leftSection = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["loginInfo"],
+    queryFn: () =>
+      fetch("http://localhost:5000/session/checkSession", {
+        method: "GET",
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  if (isPending) return <LoadingSpinner />;
+
+  if (error) return "An error has occurred: " + error.message;
   return (
     <div className="leftSection">
       <LeftIntro />
