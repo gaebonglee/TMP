04ac995e9b91be1844da6945c9c104985bcd484e
Jimmy <skawk3235@gmail.com>
2024-04-27 19:34:09 +0900

    트레이너 스케쥴 추가

--- front-end/src/components/trainerdetail/detailLeft/left/TrainerSchedule.jsx
+++ front-end/src/components/trainerdetail/detailLeft/left/TrainerSchedule.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "./TrainerSchedule.scss";
-const TrainerSchedule = () => {
+const TrainerSchedule = ({ data }) => {
   return (
     <div id="intro_page_contents_wrap" className="trainer_schedule">
       <h1>레슨 스케줄</h1>
-      <div id="wrap_container">
-        <p>test 스케줄 내용입니다.</p>
-        <p>test 스케줄 내용입니다.</p>
-      </div>
+      <ul id="wrap_container">
+        {!!data.weekday && (
+          <li className="trainerScheduleList">
+            <div className="trainerScheduleList__dayOfWeek">평일</div>
+            <div className="trainerScheduleList__time">
+              {data.weekday_start} ~ {data.weekday_end}
+            </div>
+          </li>
+        )}
+        {!!data.saturday && (
+          <li className="trainerScheduleList">
+            <div className="trainerScheduleList__dayOfWeek">토요일</div>
+            <div className="trainerScheduleList__time">
+              {data.saturday_start} ~ {data.saturday_end}
+            </div>
+          </li>
+        )}
+        {!!data.sunday && (
+          <li className="trainerScheduleList">
+            <div className="trainerScheduleList__dayOfWeek">일요일</div>
+            <div className="trainerScheduleList__time">
+              {data.sunday_start} ~ {data.sunday_end}
+            </div>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
