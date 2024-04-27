acac4e4208533efbef18baa0a7df31ca9d81e396
Jimmy <skawk3235@gmail.com>
2024-04-27 20:18:18 +0900

    데이터 연결

--- front-end/src/components/trainerdetail/detailLeft/left/CenterPlace.jsx
+++ front-end/src/components/trainerdetail/detailLeft/left/CenterPlace.jsx
@@ -2,33 +2,39 @@ import React from "react";
 import LittleUndongMap from "../../../trainermap/LittleUndongMap";
 import "./CenterPlace.scss";
 
-const CenterPlace = () => {
+const CenterPlace = ({ data }) => {
+  console.log(data);
+
+  const copyAddress = (address) => {
+    navigator.clipboard.writeText(address);
+    alert("클립보드에 주소가 복사되었습니다.");
+  };
   return (
     <div className="trainer_location_center" id="intro_page_contents_wrap">
       <h1>위치</h1>
       <div className="contentWrap">
         <div>
           <div className="flexBoxStart">
-            <strong style={{ marginRight: "8px" }}>
-              마진휘트니스 강남역점
-            </strong>
+            <strong style={{ marginRight: "8px" }}>{data.center_name}</strong>
             <div className="contact">상세 정보</div>
           </div>
           <div className="flexBoxStart">
             <div className="addressOnLocation">
-              <span style={{ marginRight: "8px" }}>
-                서울 강남구 강남대로 390 미진프라자 지하 3층 미진 휘트니스 센타
+              <span style={{ marginRight: "8px" }}>{data.center_address}</span>
+              <span
+                onClick={() => {
+                  copyAddress(data.center_address);
+                }}
+                className="contact"
+              >
+                주소 복사
               </span>
-              <span className="contact">주소 복사</span>
             </div>
-          </div>
-          <div className="addressOnLocation">
-            강남역 1번출구 10초거리 1층 스타벅스 미진프라자 B3
           </div>
         </div>
       </div>
       <div id="wrap_container">
-        <LittleUndongMap />
+        <LittleUndongMap lat={data.latitude} longi={data.longitude} />
       </div>
     </div>
   );
