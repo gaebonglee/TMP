1c894c0a53213ac820d905ea26b1d1a3b81e9324
jimmy <skawk3235@gmail.com>
2024-04-29 15:44:29 +0900

    center 관련 조회 목록 추가

--- back-end/routes/trainerDetail.js
+++ back-end/routes/trainerDetail.js
@@ -11,8 +11,11 @@ router.get("/:trainerId", async (req, res) => {
     const info3 = await sql.selectTrainerInfo3(trainerId);
     const info4 = await sql.selectTrainerInfo4(trainerId);
     const infoReview = await sql.selectTrainerInfoReview(trainerId);
+    const centerPrice = await sql.selectCenterPrice(trainerId);
 
-    res.status(200).send({ info1: info1[0], info2, info3, info4, infoReview });
+    res
+      .status(200)
+      .send({ info1: info1[0], info2, info3, info4, infoReview, centerPrice });
   } catch (err) {
     console.log("error while selectTrainerInfo1:", err);
   }
